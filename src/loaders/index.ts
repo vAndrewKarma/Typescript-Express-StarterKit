5d154ed2493256120e98e3a5cbadc6e4f4824793
Karma Andrew <karma.andrew16@gmail.com>
2023-05-23 23:02:52 +0300

    added a comment on loaders, forgot to explain

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -2,6 +2,6 @@ import expressLoader from "./express";
 import type { Express } from "express";
 import { logger } from "../config/logger";
 export default async function ({ app }: { app: Express }) {
-  await expressLoader({ app }); // loading express things
+  await expressLoader({ app }); // loading express mw, routes, handlers,etc
   logger.info("Express loaded");
 }
