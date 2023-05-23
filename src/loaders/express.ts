26f2f444a641164f1706d2d280d56ef27de2b14c
Karma Andrew <karma.andrew16@gmail.com>
2023-05-23 22:27:19 +0300

    setted up the express loader

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,1 +1,14 @@
+import express from "express";
 import type { Express } from "express";
+
+export default async function ({ app }: { app: Express }) {
+  //ROUTE FOR STATUS CHECKING
+  app.get("/alive", (req, res) => res.sendStatus(200)); // a simple status endpoint to check if the server is alive
+  app.head("/alive", (req, res) => res.sendStatus(200)); // same thing but we retrieve only response headers
+
+  app.enable("trust proxy"); // show ip which can be behind a reverse proxy
+
+  app.use(express.json()); //  used for handling encoded json data
+
+  app.use(express.urlencoded({ extended: false })); // used for handling url encoded form data like name=Example+Test&age=20
+}
