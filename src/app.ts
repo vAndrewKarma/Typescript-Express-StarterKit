c4e465435eef4ff38806305383927515ef7f1dff
Karma Andrew <karma.andrew16@gmail.com>
2023-05-23 22:09:42 +0300

    removed production if for the moment

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,18 @@ import config from "./config";
 import { logger } from "./config/logger";
 async function init() {
   const app = express();
-  if (config.env === "development") {
-    if (require.main === module) {
-      // check if the current module is the main module being run directly
-      app
-        .listen(config.app.port, () => {
-          logger.info(
-            `Server running on port ${config.app.port}  -  ENV: ${config.env}`
-          );
-        })
-        .on("error", (error) => {
-          logger.error(error.message);
-        });
-    }
+
+  if (require.main === module) {
+    // check if the current module is the main module being run directly
+    app
+      .listen(config.app.port, () => {
+        logger.info(
+          `Server running on port ${config.app.port}  -  ENV: ${config.env}`
+        );
+      })
+      .on("error", (error) => {
+        logger.error(error.message);
+      });
   }
 }
 init();
