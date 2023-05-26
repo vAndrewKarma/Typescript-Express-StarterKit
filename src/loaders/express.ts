ab40d81af5ebdf8b5708eae0f56c8d7930a33962
unknown <karma.andrew42@gmail.com>
2023-05-26 21:24:37 +0200

    added cors

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import type { Express } from "express";
 import { NotFoundHandler, GlobalErrorHandler } from "../config/routehandle";
 export default async function ({ app }: { app: Express }) {
   //ROUTE FOR STATUS CHECKING
+  const corsOptions = ["http://localhost:3001"]; // i will use a random client, but here you configure the clients which can use your api
+
+  cors(corsOptions);
   app.get("/alive", (req, res) => res.sendStatus(200)); // a simple status endpoint to check if the server is alive
   app.head("/alive", (req, res) => res.sendStatus(200)); // same thing but we retrieve only response headers
 
