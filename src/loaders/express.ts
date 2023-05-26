01921408cc8294a68f36f1249bc6d928a0d4fde0
unknown <karma.andrew42@gmail.com>
2023-05-26 22:00:11 +0200

    configured cors

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -2,11 +2,38 @@ import express from "express";
 import cors from "cors";
 import type { Express } from "express";
 import { NotFoundHandler, GlobalErrorHandler } from "../config/routehandle";
+import { logger } from "../config/logger";
 export default async function ({ app }: { app: Express }) {
-  //ROUTE FOR STATUS CHECKING
-  const corsOptions = ["http://localhost:3001"]; // i will use a random client, but here you configure the clients which can use your api
+  const clients = ["http://localhost:3000"]; // i will use a random client, but here you configure the clients which can use your api
+  const corsopt = {
+    // btw here's a tip, never use cors() cause you basically allow requests for any client, and let's say it is not the best practice if you are building an api
+    credentials: true,
+    allowedHeaders: [
+      "Authorization",
+      "Content-type",
+      "Content-Lenght",
+      "Origin",
+    ],
+    origin: function (
+      origin: string,
+      callback: (arg0: Error, arg1: boolean) => void
+    ) {
+      if (!origin) return callback(null, true);
+      if (clients.includes(origin)) {
+        // if origin exists in clients
+        callback(null, true);
+      } else {
+        // error logging
+        const error = new Error(`${origin} not allowed`);
+        logger.error(error);
+        callback(error, false);
+      }
+    },
+  };
 
-  cors(corsOptions);
+  app.use(cors(corsopt));
+
+  //ROUTE FOR STATUS CHECKING
   app.get("/alive", (req, res) => res.sendStatus(200)); // a simple status endpoint to check if the server is alive
   app.head("/alive", (req, res) => res.sendStatus(200)); // same thing but we retrieve only response headers
 
